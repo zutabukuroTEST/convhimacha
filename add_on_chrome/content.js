8e30a103a158bd3a8173be9cff43f89460c1b279
mymta <miyamotoak1011@gmail.com>
2020-12-31 17:09:58 +0900

    初回

---
+++ add_on_chrome/content.js
@@ -0,0 +1,37 @@
+//対象ID(募集欄から消したいユーザーID)
+const targetsID = [
+    33393,
+    42200];
+
+//監視設定
+const CONFIG = {
+    childList: true,
+    attribute: true,
+    subtree: true
+};
+
+//トップ画面監視開始
+const layer_observer = new MutationObserver(stream);
+layer_observer.observe(document.getElementById('co_layerroot'), CONFIG);
+
+//募集欄監視開始
+function stream() {
+    if(document.getElementsByClassName('layer layer_yobikomi')[0] != null) {
+        const list_observer = new MutationObserver(hide);
+        list_observer.observe(document.getElementsByClassName('layer layer_yobikomi')[0], CONFIG);
+    }
+}
+
+//削除
+function hide() {
+    const collection = document.getElementsByClassName('yobikomiul');
+    const list = Array.prototype.slice.call(collection);
+    list.forEach(element => {
+       const elemID = element.getAttribute('onclick').replace(/[^0-9]/g, "");
+       targets.forEach(targetID => {
+           if(elemID == targetID) {
+               element.remove();
+           }
+       });
+    });
+}
