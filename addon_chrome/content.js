11c72cd14be5b9e78dd0669926dfe515bcfd8138
mymta <miyamotoak1011@gmail.com>
2020-12-31 17:34:03 +0900

    ディレクトリ名変更

---
+++ addon_chrome/content.js
@@ -0,0 +1,43 @@
+//対象ID(募集欄から消したいユーザーID)
+/*
+const targetsID= [
+    1,
+    2,
+    3,
+]のように募集欄から消したいユーザーIDを追加してください。
+*/
+const targetsID = [
+    42200];
+
+//監視設定
+const CONFIG = {
+    childList: true,
+    attribute: true,
+    subtree: true
+};
+
+//トップ画面監視開始
+const layer_observer = new MutationObserver(stream);
+layer_observer.observe(document.getElementById('co_layerroot'), CONFIG);
+
+//募集欄監視開始
+function stream() {
+    if(document.getElementsByClassName('layer layer_yobikomi')[0] != null) {
+        const list_observer = new MutationObserver(hide);
+        list_observer.observe(document.getElementsByClassName('layer layer_yobikomi')[0], CONFIG);
+    }
+}
+
+//削除
+function hide() {
+    const collection = document.getElementsByClassName('yobikomiul');
+    const list = Array.prototype.slice.call(collection);
+    list.forEach(element => {
+       const elemID = element.getAttribute('onclick').replace(/[^0-9]/g, "");
+       targets.forEach(targetID => {
+           if(elemID == targetID) {
+               element.remove();
+           }
+       });
+    });
+}
