234ca23178123d8e6f0837f801306bb06bef9500
zutabukuroTEST <miyamotoak1011@gmail.com>
2021-07-07 23:46:39 +0900

    機能追加

--- addon_chrome/content.js
+++ addon_chrome/content.js
@@ -20,19 +20,28 @@ const CONFIG = {
     subtree: true
 };
 
-//トップ画面監視開始
+$(window).load(function() {
+    console.log(window.myid);
+})
+
+//監視開始
 const layer_observer = new MutationObserver(stream);
 layer_observer.observe(document.getElementById('co_layerroot'), CONFIG);
 
-//募集欄監視開始
+
+//募集欄監視
 function stream() {
     if(document.getElementsByClassName('layer layer_yobikomi')[0] != null) {
         const list_observer = new MutationObserver(hide);
         list_observer.observe(document.getElementsByClassName('layer layer_yobikomi')[0], CONFIG);
     }
+    if(document.getElementsByClassName('sourcespace')[0] != null) {
+        const boshu_observer = new MutationObserver(dispData);
+        boshu_observer.observe(document.getElementsByClassName('sourcespace')[0], CONFIG);
+    }
 }
 
-//削除
+//対象IDに該当するユーザを非表示
 function hide() {
     const collection = document.getElementsByClassName('yobikomiul');
     const list = Array.prototype.slice.call(collection);
@@ -44,4 +53,22 @@ function hide() {
            }
        });
     });
+}
+
+//アカウント情報表示
+function dispData() {
+    //表示用span作成
+    $(function() {
+            $('.yobikomi_form').append('<span></span>')
+            $('.yobikomi_form').find('span').css({'background-color':'white'});
+    })
+    //表示
+    $('.yobikomiul').each(function(){
+        $(this).mouseover(function() {
+            $('.yobikomi_form').find('span').eq(0).text('ユーザ名:' + $(this).find('span').eq(0).text())
+            $('.yobikomi_form').find('span').eq(1).text('ユーザID:' + $(this).attr('onclick').replace(/[^0-9]/g, ""))
+        }).mouseout(function() {
+            $('.yobikomi_form').find('span').text('');
+        })
+    })
 }
